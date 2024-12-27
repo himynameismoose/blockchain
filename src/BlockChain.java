5b3dd460fa028124c41ccf6f27b336d5fc05c010
Mershelle Rivera <mershellemae@gmail.com>
2024-12-26 16:11:35 -0800

    implement ArrayList to view JSON

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -1,12 +1,18 @@
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+
 public class BlockChain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+
     public static void main(String[] args) {
-        Block genesisBlock = new Block("Hi, I'm the first block", "0");
-        System.out.println("Hash for block 1: " + genesisBlock.hash);
+        // Add blocks to blockchain ArrayList
+        blockchain.add(new Block("Hi, I'm the first block", "0"));
+        blockchain.add(new Block("Yo, I'm the second block", blockchain.get(blockchain.size() - 1).hash));
+        blockchain.add(new Block("Hey, I'm the third block", blockchain.get(blockchain.size() - 1).hash));
 
-        Block secondBlock = new Block("Yo, I'm the second block", genesisBlock.hash);
-        System.out.println("Hash for block 2: " + secondBlock.hash);
-
-        Block thirdBlock = new Block("Hey, I'm the third block", secondBlock.hash);
-        System.out.println("Hash for block 3: " + thirdBlock.hash);
+        String blockchainJSON = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJSON);
     }
 }
