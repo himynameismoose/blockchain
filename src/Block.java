c31490e5357667a49a7cffe05239c75375b4ec1c
Mershelle Rivera <mershellemae@gmail.com>
2024-12-26 17:25:39 -0800

    add variable nonce and mineBlock()

--- src/Block.java
+++ src/Block.java
@@ -5,6 +5,7 @@ public class Block {
     public String previousHash;
     private String data; // message
     private long timeStamp; // in ms
+    private int nonce;
 
     // constructor
     public Block(String data, String previousHash) {
@@ -14,7 +15,19 @@ public class Block {
         this.hash = calculateHash();
     }
 
+    // Calculate new hash based on block contents
     public String calculateHash() {
-        return StringUtil.applySHA256(previousHash + Long.toString(timeStamp) + data);
+        return StringUtil.applySHA256(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
+    }
+
+    public void mineBlock(int difficulty) {
+        // Create a String with difficulty * "0"
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+
+        System.out.println("Block Mined!!!: " + hash);
     }
 }
