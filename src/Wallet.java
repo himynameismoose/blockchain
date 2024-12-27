c0b56fd344b1763d6804b5e820da3dc324e53b0d
Mershelle Rivera <mershellemae@gmail.com>
2024-12-27 11:24:19 -0800

    implement elliptic curve keypair

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,7 +1,30 @@
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
     public PrivateKey privateKey;
     public PublicKey publicKey;
+
+    public Wallet() {
+        generateKeyPair();
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("prime192v1");
+
+            // Initiate the key generator and generate a KeyPair
+            keyPairGenerator.initialize(ecGenParameterSpec, secureRandom);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+            // Set the public and private keys from the keyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
