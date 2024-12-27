a7a3393086268404219eb538817f63b1e78df1a1
Mershelle Rivera <mershellemae@gmail.com>
2024-12-27 11:48:43 -0800

    create helper functions to store signature

--- src/StringUtil.java
+++ src/StringUtil.java
@@ -1,4 +1,8 @@
+import java.security.Key;
 import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.util.Base64;
 
 public class StringUtil {
     // Apply SHA256 to a String and return result
@@ -24,5 +28,26 @@ public class StringUtil {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // Apply ECDSA Signature and return result as bytes
+    public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+        Signature dsa;
+        byte[] output = new byte[0];
+        try {
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+            output = dsa.sign();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return output;
+    }
+
+    public static String getStringFromKey(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }
