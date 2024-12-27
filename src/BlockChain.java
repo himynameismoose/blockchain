d2e5690399585a13b5fe6d01521de9be5c4cf128
Mershelle Rivera <mershellemae@gmail.com>
2024-12-26 16:25:04 -0800

    add method to check if the blockchain is valid

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -15,4 +15,29 @@ public class BlockChain {
         String blockchainJSON = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJSON);
     }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        // Loop through blockchain to check hashes
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+
+            // Compare registered hash and calculated hash
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+
+            // Compare previous hash and registered previous hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
