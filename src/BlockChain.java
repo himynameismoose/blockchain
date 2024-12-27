f4426a626cf343a178940715e8dd73533ecc2422
Mershelle Rivera <mershellemae@gmail.com>
2024-12-27 11:10:26 -0800

    testing difficulty variable

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -5,20 +5,33 @@ import java.util.ArrayList;
 public class BlockChain {
 
     public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 5;
 
     public static void main(String[] args) {
         // Add blocks to blockchain ArrayList
         blockchain.add(new Block("Hi, I'm the first block", "0"));
+        System.out.println("Trying to Mine Block 1...");
+        blockchain.get(0).mineBlock(difficulty);
+
         blockchain.add(new Block("Yo, I'm the second block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to Mine Block 2...");
+        blockchain.get(1).mineBlock(difficulty);
+
         blockchain.add(new Block("Hey, I'm the third block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to Mine Block 3...");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid());
 
         String blockchainJSON = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
         System.out.println(blockchainJSON);
     }
 
     public static Boolean isChainValid() {
         Block currentBlock;
         Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
         // Loop through blockchain to check hashes
         for (int i = 1; i < blockchain.size(); i++) {
@@ -34,6 +47,12 @@ public class BlockChain {
             // Compare previous hash and registered previous hash
             if (!previousBlock.hash.equals(currentBlock.previousHash)) {
                 System.out.println("Previous Hashes not equal");
+                return false;
+            }
+
+            // Check if hash is solved
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
                 return false;
             }
         }
